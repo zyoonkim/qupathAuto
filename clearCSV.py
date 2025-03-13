33c7350230576c18265296ea0d458991080b8294
zyoonkim <zyoonkim@umich.edu>
2025-03-13 14:43:45 -0400

    files are cooked, trying to fix this

---
+++ clearCSV.py
@@ -0,0 +1,8 @@
+# Wipe the contents of a CSV file
+csv_file_path = '/Users/zyoonkim/cs/qupathAutomation/analysisProject/cell_detection_results.csv'
+
+# Open the file in write mode to clear it
+with open(csv_file_path, 'w') as file:
+    file.truncate(0)
+
+print(f"{csv_file_path} has been wiped clean.")
