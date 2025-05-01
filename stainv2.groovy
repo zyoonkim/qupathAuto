e1e709b87b9dc565245e194b472525d326894d31
zyoonkim <zyoonkim@umich.edu>
2025-05-01 17:49:28 -0400

    final push for real

--- stainv2.groovy
+++ stainv2.groovy
@@ -22,6 +22,7 @@ for (entry in getProject().getImageList()) {
     // Now set stain vector (H-DAB)
     setColorDeconvolutionStains('{"Name" : "H-DAB default", "Stain 1" : "Hematoxylin", "Values 1" : "0.65111 0.701190 0.29049", "Stain 2" : "DAB", "Values 2" : "0.26917 0.568240 0.77759", "Background" : "255 255 255"}')
 
+
     // Run positive cell detection
     runPlugin('qupath.imagej.detect.cells.PositiveCellDetection',
     '{"detectionImageBrightfield": "Optical density sum", ' +
